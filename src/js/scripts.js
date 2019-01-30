5d4aa0832bc6b8b0d765af094fd1a44e3197b97f
Yegor Zaporozhets <egorserg4@gmail.com>
2019-01-30 19:56:06 +0300

    Complete mobile, begin tablet and desktop

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,22 @@
-// Your code here...
+$('.slick-slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    prevArrow: '<span class="arrow-prev"><</span>',
+    nextArrow: '<span class="arrow-next">></span>',
+    responsive: [
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: 'unslick'
+        }
+    ]
+});
